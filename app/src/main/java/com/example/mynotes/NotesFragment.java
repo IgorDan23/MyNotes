ea38427cc2d5433e80c70e4dec350a32bd4095d1
igor_ <igor_dan@inbox.ru>
2022-02-16 17:57:59 +0300

    Домашнее задание 8. Во фрагменте с заметкой добавил пункт меню. И скрыл about при открытии фрагмента.

--- app/src/main/java/com/example/mynotes/NotesFragment.java
+++ app/src/main/java/com/example/mynotes/NotesFragment.java
@@ -8,10 +8,14 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -27,6 +31,21 @@ public class NotesFragment extends Fragment {
         return fragment;
     }
 
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_menu,menu);
+        menu.findItem(R.id.action_about).setVisible(false);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case (R.id.action_toast):
+                Toast.makeText(getContext(),"Ваша заметка.", Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,6 +56,7 @@ public class NotesFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setHasOptionsMenu(true);
         notes = getArguments().getParcelable(KEY);
         getChildFragmentManager().beginTransaction()
                 .replace(R.id.child_frame, NotesFragmentChild.newInstance(notes))
